54f4310360f70ba36b9b5c29733f6d0b049437e5
sotarotanaka <strunv0302@gmail.com>
2020-01-05 18:21:21 +0900

    変更あり

---
+++ Brockbreak/Assets/Ball_move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody> ();
+        Vector3 force = new Vector3 (300.0f,0.0f,-600.0f);
+        rb.AddForce (force); 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="Box"){
+            Destroy(collision.gameObject);
+        }
+    }
+}
